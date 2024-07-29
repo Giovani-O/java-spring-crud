680a54d5b90b2079948414a6844424d6cbc8f66d
Giovani <giovani98@proton.me>
2024-07-29 19:20:12 -0300

    feat: WiP - Creating controller

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,4 +1,16 @@
 package com.example.crud.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Anotação que define que essa classe é um controlador REST
+@RequestMapping("/product") // Define a rota base para esse controlador
 public class ProductController {
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok("Ok");
+    }
 }
+
